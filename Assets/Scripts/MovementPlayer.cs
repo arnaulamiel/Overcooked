00749e98d11817613c29ac8cdd6d46774ceeb391
Alex Rubiano Márquez <58170615+alexrubmar@users.noreply.github.com>
2021-05-07 21:59:52 +0200

    movimiento de jugador funciona fluido

---
+++ Assets/Scripts/MovementPlayer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementPlayer : MonoBehaviour
+{
+    public float speed = 10f;
+
+    [SerializeField]
+    private Rigidbody rBody;
+    private Vector3 moveDir;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float xDir = Input.GetAxis("Horizontal");
+        float zDir = Input.GetAxis("Vertical");
+
+        moveDir = new Vector3(xDir, 0.0f, zDir) * speed;
+        transform.LookAt(transform.position + new Vector3(-moveDir.x, 0, -moveDir.z));
+
+    }
+
+    private void FixedUpdate()
+    {
+        rBody.velocity = moveDir;
+    }
+}

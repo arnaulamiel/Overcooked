565472dce5e24c2eaaad02e53ddfc26cf6295e5d
arnaulamiel <arnau.lamiel@estudiantat.upc.edu>
2021-05-28 15:15:51 +0200

    Pantalla teclas god mode

--- Assets/Scripts/BackHelpScript.cs
+++ Assets/Scripts/BackHelpScript.cs
@@ -4,29 +4,20 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class BackScript : MonoBehaviour
+public class BackHelpScript : MonoBehaviour
 {
-    public Button help, godmode;
+    public Button help;
     Transform childh;
-
     // Start is called before the first frame update
     void Start()
     {
         childh = transform.Find("Button");
         Button btn2 = childh.GetComponent<Button>();
         btn2.onClick.AddListener(TaskOnClickHelp);
-
-        childh = transform.Find("godmode");
-        godmode = childh.GetComponent<Button>();
-        godmode.onClick.AddListener(TaskOnClickGOD);
-
     }
+
     void TaskOnClickHelp()
     {
-        SceneManager.LoadScene(sceneName: "MenuScene");
-    }
-    void TaskOnClickGOD()
-    {
-        SceneManager.LoadScene(sceneName: "GodMode");
+        SceneManager.LoadScene(sceneName: "HelpScene");
     }
 }
